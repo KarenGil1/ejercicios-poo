e891b0d86d7edab845c48c1203a7da08ada27c3a
Ivan Gil <ivan.gil@gammapartners.com>
2021-04-19 16:54:54 -0600

    Subir proyecto

---
+++ src/ejerciciosgithub.java
@@ -0,0 +1,7 @@
+public class ejerciciosgithub {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Mi nombre es Karen Vanessa Gil Najera");
+        System.out.println("mis iniciales son KVGN");
+    }
+}
