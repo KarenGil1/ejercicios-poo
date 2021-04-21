d75db9c745a5e2f3c2da9d8bc7e874edc7e6f1ca
Ksren Gil <l20550352@chihuahua2.tecnm.mx>
2021-04-21 12:39:20 -0600

    Subir proyecto

--- src/ejerciciosgithub.java
+++ src/ejerciciosgithub.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class ejerciciosgithub {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Mi nombre es Karen Vanessa Gil Najera");
         System.out.println("mis iniciales son KVGN");
+
+        Scanner escribir=new Scanner(System.in);
+        int num1, num2;
+        System.out.println("dame un numero");
+        num1=escribir.nextInt();
+        System.out.println("dame otro numero");
+        num2=escribir.nextInt();
+        System.out.println("Los numeros que me diste son: "+num1+" y "+num2);
+
+
     }
 }
