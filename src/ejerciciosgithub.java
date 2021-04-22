48b24c8a7836df5b939edfcc09178761d3eff3ad
Ksren Gil <l20550352@chihuahua2.tecnm.mx>
2021-04-22 13:11:24 -0600

    pronostico

--- src/ejerciciosgithub.java
+++ src/ejerciciosgithub.java
@@ -1,19 +1,13 @@
 import java.util.Scanner;
 public class ejerciciosgithub {
-    public static void main(String [] args) {
-        System.out.println("Hola mundo");
-        System.out.println("Mi nombre es Karen Vanessa Gil Najera");
-        System.out.println("mis iniciales son KVGN");
-
-            Scanner escribir = new Scanner(System.in);
-            int num1, num2;
-            System.out.println("Para probar dame un numero");
-            num1 = escribir.nextInt();
-            System.out.println("dame otro numero");
-            num2 = escribir.nextInt();
-            System.out.println("Los numeros que me diste son: " + num1 + " y " + num2);
-            System.out.println("SI FUNCIONA");
+    public static void main(String[] args) {
+            String cabecera = "\n\t PRONOSTICO DE CLIMA:";
+            cabecera += "\n\tDia\t\t mañana\t\t noche\t\tcondiciones\n";
+            cabecera += "\t-----\t\t-------\t--------\t----------\n";
+            String pronostico = "\tDomingo\t25C\t\t33C\t\tSoleado\n";
+            pronostico += "\tLunes\t15C\t\t23C\t\tnublado\n";
+            pronostico += "\tMartes\t25C\t\t13C\t\tCaluroso\n";
+            System.out.print(cabecera + pronostico);
         }
-
     }
-
+    
